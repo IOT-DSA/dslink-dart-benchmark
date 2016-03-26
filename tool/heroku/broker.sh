d48a6e5db85cf2da8a97c83bff6c342264eff605
Kenneth Endfinger <k.endfinger@dglogik.com>
2016-03-25 20:24:50 -0700

    Update to use C Broker

--- tool/heroku/broker.sh
+++ tool/heroku/broker.sh
@@ -4,4 +4,18 @@ export BROKER_PORT=${PORT}
 
 rm -rf conns.json broker.json tmp
 
-dart .pub/bin/dslink/broker.dart.snapshot --docker
+wget https://ci.dev.dglogik.com/repository/download/Dsa_CBroker/.lastSuccessful/linux_x64_broker --http-user guest --http-passwd guest -O broker
+chmod +x broker
+
+cat <<EOF > broker.json
+{
+  "http": {
+    "enabled": true,
+    "host": "0.0.0.0",
+    "port": ${BROKER_PORT}
+  },
+  "log_level": "info"
+}
+EOF
+
+./broker
